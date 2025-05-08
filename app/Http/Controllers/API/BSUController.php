88049c04108f673a1fa2964680f217e3c2783565
trinantaasembiring <kurniawan.rizki.2305356@students.um.ac.id>
2025-05-08 09:50:29 +0700

    asd

--- app/Http/Controllers/API/BSUController.php
+++ app/Http/Controllers/API/BSUController.php
@@ -142,7 +142,7 @@ class BSUController extends Controller
                 'nik_list' => implode(',', $nikList)
             ]);
             
-            $nasabahData = $response->json()['data'];
+            $nasabahData = $response->json();
             return $nasabahData;
             // Gabungkan data transaksi dengan data nasabah
             $result = $topContributors->map(function ($contributor) use ($nasabahData) {
