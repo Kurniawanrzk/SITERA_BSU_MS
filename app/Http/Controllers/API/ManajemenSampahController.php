54b7da77b73e36eaaed3a7b95b7cf355be636a2a
trinantaasembiring <kurniawan.rizki.2305356@students.um.ac.id>
2025-05-09 00:45:57 +0700

    asda

--- app/Http/Controllers/API/ManajemenSampahController.php
+++ app/Http/Controllers/API/ManajemenSampahController.php
@@ -98,8 +98,7 @@ class ManajemenSampahController extends Controller
     public function cekSemuaTransaksiBSU(Request $request)
     {
         $token = $request->get("token");
-        $perPage = $request->get('per_page', 10); // default 10 data per halaman
-        $transaksi = Transaksi::with('detailTransaksi.sampah')->paginate($perPage);
+        $transaksi = Transaksi::with('detailTransaksi.sampah')->get();
     
         return response()->json([
             "status" => true,
